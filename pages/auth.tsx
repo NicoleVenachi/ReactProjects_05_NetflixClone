ee8803ad8052303dfb35d29916300d04b6ed2104
nicolevenachi <ludwing@unicauca.edu.co>
2023-08-24 07:50:52 -0500

    Auth Screen UI - finised (toggle register and login logic)

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,12 +1,18 @@
 import Input from "@/components/input"
 import Image from "next/image"
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
 
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+
+  const [variant, setVariant] = useState('login'); //variant toggle entre login y register
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -18,18 +24,22 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sign in
-              {/* {variant === 'login' ? 'Sign in' : 'Register'} */}
+              {variant === 'login' ? 'Sign in' : 'Register'}
+              {/* muestro texto para login o para register */}
             </h2>
 
             <div className="flex flex-col gap-4">
-              <Input
-                id="name"
-                type="text"
-                label="Username"
-                value={name}
-                onChange={(e: any) => setName(e.target.value)}
-              />
+              {variant === 'register' && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              )}
+
+              {/* muestro o no el username si estoy o no registrado */}
 
               <Input
                 id="email"
@@ -38,7 +48,6 @@ const Auth = () => {
                 value={email} //muetro state
                 onChange={(e: any) => setEmail(e.target.value)} //meto en el state el valor escrito en input
               />
-
               <Input
                 type="password"
                 id="password"
@@ -47,6 +56,19 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
+
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                {variant === 'login' ? 'Create an account' : 'Login'}
+              </span>
+              .
+              {/* clickeando el span toggle entre lgoin y register */}
+            </p>
 
           </div>
         </div>
